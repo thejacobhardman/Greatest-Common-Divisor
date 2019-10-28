14dbcd1a7a3488249c17e550bdb41ccc4e8c527d
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2019-10-27 22:01:51 -0600

    add program

--- Greatest-Common-Divisor/Program.cs
+++ Greatest-Common-Divisor/Program.cs
@@ -10,7 +10,45 @@ namespace Greatest_Common_Divisor
     {
         static void Main(string[] args)
         {
+            int num1, num2, greatestCommon;
 
+            do
+            {
+                bool goodInput;
+                do
+                {
+                    Console.Write("Enter first number (-1 To Exit): ");
+                    goodInput = int.TryParse(Console.ReadLine(), out num1);
+                } while (!goodInput);
+
+                if (num1 == -1) { break; }
+
+                do
+                {
+                    Console.Write("Enter second number: ");
+                    goodInput = int.TryParse(Console.ReadLine(), out num2);
+                } while (!goodInput);
+
+                greatestCommon = GCD(num1, num2);
+
+                Console.WriteLine("GCD is: " + greatestCommon + "\n");
+            } while (num1 != -1);
+
+            Console.WriteLine("Press any key to close the program.");
+            Console.ReadKey();
+        }
+
+        static int GCD(int num1, int num2)
+        {
+            while (num1 != 0 && num2 != 0)
+            {
+                if (num1 > num2)
+                    num1 %= num2;
+                else
+                    num2 %= num1;
+            }
+
+            return num1 == 0 ? num2 : num1;
         }
     }
 }
